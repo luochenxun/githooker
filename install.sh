1c71bb4c4a729e93c2be27aaf7299d6e2d38faa0
luochenxun <chenxun.luo@jyblife.com>
2017-11-24 11:55:49 +0800

    test

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
-GITHOOKER_HOME="~/.githookkk"
+USER_HOME=`echo ~`
+GITHOOKER_HOME=$USER_HOME"/.githookkk"
 GITHOOKER_NAME="githookkk"
 
 main() {
   git clone https://github.com/luochenxun/githooker.git $GITHOOKER_HOME
 
-  sudo mv GITHOOKER_HOME/githookkk /usr/local/bin/$GITHOOKER_NAME
+  echo "Install will move the githooker to your /usr/local/bin/ directory, please input your system password"
+  sudo mv $GITHOOKER_HOME/githookkk /usr/local/bin/$GITHOOKER_NAME
   sudo chmod a+x /usr/local/bin/$GITHOOKER_NAME
   echo "Congratulation, githooker has installed successfully!"
 }
 
 
 main
-
